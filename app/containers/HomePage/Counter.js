98d93311132c83d803e58b1c6b354a485e3d1b1b
gagaduday <ladygaga191195@gmail.com>
2019-08-26 15:07:12 +0700

    redux counter

--- app/containers/HomePage/Counter.js
+++ app/containers/HomePage/Counter.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { increase, decrease } from './actions';
 
-const Counter = props => (
-  <div>
-    <button onClick={props.increase}>Increment</button>
-    <button onClick={props.decrease}>Decrement</button>
-    <p>Current count</p>
-    {props.count}
-  </div>
-);
+const Counter = props => {
+  console.log(props);
+  return (
+    <div>
+      <button onClick={props.increase}>Increment</button>
+      <button onClick={props.decrease}>Decrement</button>
+      <p>Current count</p>
+      {props.count}
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({
-  count: state.count,
-});
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    count: state.home.count.count,
+  };
+};
 
 export default connect(
   mapStateToProps,
